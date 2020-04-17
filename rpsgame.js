97521d0ea7e2227f15bb777771991135ba0ab303
tamaszentai <zentaitamas@gmail.com>
2020-04-17 14:53:21 +0100

    Game stops when player/computer reaches 0 souls

--- rpsgame.js
+++ rpsgame.js
@@ -24,55 +24,68 @@ var scissor = document.querySelector(".scissordiv");
 
 rock.addEventListener("click", function(){
     userChoice = 'rock';
-    game();
+    stopGameIfZeroLife();
 });
 
 paper.addEventListener("click", function(){
     userChoice = 'paper';
-    game();
+    stopGameIfZeroLife();
 });
 scissors.addEventListener("click", function(){
     userChoice = 'scissors';
-    game();
+    stopGameIfZeroLife();
 });
 
 
 
 function game(){
     computerSelection();
+    
  switch(userChoice + computerChoice){
      case 'rockscissors':
-        document.querySelector(".textdiv").textContent = 'Rock beats Scissors, ' + playerName + ' wins!';
+        document.querySelector(".textdiv").textContent = 'Rock vs. Scissors, ' + playerName + ' wins!';
         document.querySelector(".computerlivescontainer").textContent = computerLife -= 1;
         break;
      case 'paperrock':
-        document.querySelector(".textdiv").textContent = 'Paper beats Rock, ' + playerName + ' wins!';
+        document.querySelector(".textdiv").textContent = 'Paper vs. Rock, ' + playerName + ' wins!';
         document.querySelector(".computerlivescontainer").textContent = computerLife -= 1;
         break;
      case 'scissorspaper':
-        document.querySelector(".textdiv").textContent = 'Scissors beats Paper, ' + playerName + ' wins!';
+        document.querySelector(".textdiv").textContent = 'Scissors vs. Paper, ' + playerName + ' wins!';
         document.querySelector(".computerlivescontainer").textContent = computerLife -= 1;
         break;
     case 'scissorsrock':
-        document.querySelector(".textdiv").textContent = 'Scissors beaten by Rock, Computer wins!';
+        document.querySelector(".textdiv").textContent = 'Scissors vs. Rock, Computer wins!';
         document.querySelector(".playerlivescontainer").textContent = playerLife -= 1;
         break;
     case 'rockpaper':
-        document.querySelector(".textdiv").textContent = 'Rock beaten by Paper, Computer wins!';
+        document.querySelector(".textdiv").textContent = 'Rock vs. Paper, Computer wins!';
         document.querySelector(".playerlivescontainer").textContent = playerLife -= 1;
         break;
     case 'paperscissors':
-        document.querySelector(".textdiv").textContent = 'Paper beaten by Scissors, Computer wins!';
+        document.querySelector(".textdiv").textContent = 'Paper vs. Scissors, Computer wins!';
         document.querySelector(".playerlivescontainer").textContent = playerLife -= 1;
         break;
     case 'rockrock':
-        document.querySelector(".textdiv").textContent = 'Rock and Rock, Draw!'; 
+        document.querySelector(".textdiv").textContent = 'Rock vs. Rock, Draw!'; 
         break;
     case 'paperpaper':
-        document.querySelector(".textdiv").textContent = 'Paper and Paper, Draw!'; 
+        document.querySelector(".textdiv").textContent = 'Paper vs. Paper, Draw!'; 
         break;
     case 'scissorsscissors':
-        document.querySelector(".textdiv").textContent = 'Scissors and Scissors, Draw!';
+        document.querySelector(".textdiv").textContent = 'Scissors vs. Scissors, Draw!';
         break;
+    }
+    if (playerLife === 0){
+        setTimeout(function(){ document.querySelector(".textdiv").textContent = 'Computer Wins!'; }, 1500); 
+    } else if (computerLife === 0){
+        setTimeout(function(){ document.querySelector(".textdiv").textContent = playerName + ' Wins!';}, 1500);
+    }
+}
+
+
+function stopGameIfZeroLife(){
+    if (playerLife && computerLife !==0){
+        game();
     }
 }
